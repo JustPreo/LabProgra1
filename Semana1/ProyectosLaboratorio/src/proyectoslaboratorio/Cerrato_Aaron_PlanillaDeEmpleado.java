50c06bb791a7b920979af2ec697c29e871b78354
JustPreo <impre0hn@gmail.com>
2025-04-25 13:40:31 -0600

    PlantillaDeEmpleado

---
+++ Semana1/ProyectosLaboratorio/src/proyectoslaboratorio/Cerrato_Aaron_PlanillaDeEmpleado.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoslaboratorio;
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+
+
+/* Objetivo del programa
+Conociendo las horas laboradas al mes y la tarifa por hora
+Sacar el salario mensual
+Pedir Nombre de empleado
+Horas trabajadas en el mes
+
+
+*/
+public class Cerrato_Aaron_PlanillaDeEmpleado {
+    public static void main(String[]Args)
+    {
+    Scanner Input = new Scanner(System.in);
+    
+    System.out.println("***Bienvenido***");
+    System.out.println("Ingrese el nombre del empleado");
+    String nombre = Input.nextLine();
+    System.out.println("Ingrese las horas trabajadas en el mes");
+    int HorasMes = Input.nextInt();
+    Input.nextLine();
+    System.out.println("Ingrese la tarifa por hora(En Lempiras)");
+    double TarifaHora = Input.nextDouble();
+    
+    //Calcular las variables
+    double PagoMensual = HorasMes * TarifaHora;
+    
+    double PagoSemanal = PagoMensual/4;
+    
+    System.out.println("----- Boleta del Empleado -------");
+    System.out.println("Nombre del empleado: "+nombre+
+            "\nHoras de Trabajo Mensual: "+HorasMes+
+            "\nTarifa por Hora :Lps."+TarifaHora+
+            "\nSalario del Empleado Semanal:Lps."+PagoSemanal);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
