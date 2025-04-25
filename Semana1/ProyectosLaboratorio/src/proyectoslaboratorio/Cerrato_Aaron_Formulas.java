88f1217f52685698335c4bd99f474345e086acf7
JustPreo <impre0hn@gmail.com>
2025-04-25 16:58:33 -0600

    Formulas

---
+++ Semana1/ProyectosLaboratorio/src/proyectoslaboratorio/Cerrato_Aaron_Formulas.java
@@ -0,0 +1,163 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoslaboratorio;
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class Cerrato_Aaron_Formulas {
+    public static void main(String[]args)
+    {
+    double a,b,c,d,e,f,g,h,j;
+    Scanner In = new Scanner(System.in);
+        System.out.println("**Ejercicio 1**");
+        double ejercicioa = (3/2)+(4/3);
+        System.out.println("La respuesta del ejercicio 1 es: "+ejercicioa);
+        
+        System.out.println("**Ejercicio 2**");
+        System.out.println("Ingrese la variable x");
+        double x = In.nextDouble();
+        System.out.println("Ingrese la variable y");
+        double y = In.nextDouble();
+        double ejerciciob = (1/(x-5))-((3*x*y)/4);
+        System.out.println("La respuesta del ejercicio 2 es: "+ejerciciob);
+        
+        System.out.println("**Ejercicio 3**");
+        double ejercicioc = (1/2)+7;
+        System.out.println("La respuesta del ejercicio c es: "+ejercicioc);
+        
+        System.out.println("**Ejercicio 4**");
+        double ejerciciod = 7+(1/2);
+        System.out.println("La respuesta del ejercicio d es: "+ejerciciod);
+        
+        System.out.println("**Ejercicio 4**");
+        System.out.println("Ingrese la variable a");
+        a = In.nextDouble();
+        System.out.println("Ingrese la variable b");        
+        b = In.nextDouble();
+        System.out.println("Ingrese la variable c");        
+        c = In.nextDouble();
+        System.out.println("Ingrese la variable d");        
+        d = In.nextDouble();
+        System.out.println("Ingrese la variable e");        
+        e = In.nextDouble();
+        System.out.println("Ingrese la variable f");        
+        f = In.nextDouble();
+        System.out.println("Ingrese la variable g");        
+        g = In.nextDouble();
+        System.out.println("Ingrese la variable h");        
+        h = In.nextDouble();
+        System.out.println("Ingrese la variable j");        
+        j = In.nextDouble();       
+        
+        double ejercicioe = (Math.pow(a,2)/(b-c)+((d-3)/(f-((g*h)/j))));
+        
+        System.out.println("La respuesta del ejercicio e es: "+ejercicioe);
+        
+        System.out.println("**Ejercicio 6**");
+        double m,n,p;
+        System.out.println("Ingrese la variable m");
+        m = In.nextDouble();
+        System.out.println("Ingrese la variable n");
+        n = In.nextDouble();
+        System.out.println("Ingrese la variable p");
+        p = In.nextDouble();
+        
+        double ejerciciof = (m/n)+p;
+        System.out.println("La respuesta del ejercicio f es: "+ejerciciof);
+        
+        System.out.println("**Ejercicio 7**");
+        double q;
+        System.out.println("Ingrese la variable m");
+        System.out.println("Ingrese la variable m");
+        m = In.nextDouble();
+        System.out.println("Ingrese la variable n");
+        n = In.nextDouble();
+        System.out.println("Ingrese la variable p");
+        p = In.nextDouble();
+        System.out.println("Ingrese la variable q");
+        q = In.nextDouble();
+        
+        double ejerciciog = m+(n/(p-q));
+        System.out.println("La respuesta del ejercicio g es: "+ejerciciog);
+        System.out.println("**Ejercicio 8**");
+        
+        System.out.println("Ingrese la variable a");
+        a = In.nextDouble();
+        System.out.println("Ingrese la variable b");
+        b = In.nextDouble();
+        System.out.println("Ingrese la variable c");
+        c = In.nextDouble();
+        System.out.println("Ingrese la variable d");
+        d = In.nextDouble();
+        
+        double ejercicioh = ((Math.pow(a, 2))/Math.pow(b, 2)+(Math.pow(c, 2)/Math.pow(d, 2)));
+        System.out.println("El resultado del ejercicio h es: "+ejercicioh);
+        
+        System.out.println("**Ejercicio 9**");
+        double r,s;
+        System.out.println("Ingrese la variable m");
+        m = In.nextDouble();
+        System.out.println("Ingrese la variable n");
+        n = In.nextDouble();
+        System.out.println("Ingrese la variable p");
+        p = In.nextDouble();
+        System.out.println("Ingrese la variable q");
+        q = In.nextDouble();
+        System.out.println("Ingrese la variable r");
+        r = In.nextDouble();
+        System.out.println("Ingrese la variable s");
+        s = In.nextDouble();
+        double ejercicioi = (m+(n/p)/(q-(r/s)));
+        System.out.println("El resultado del ejercicio i es: "+ejercicioi);
+        
+        
+        System.out.println("Ingrese la variable a");
+        a = In.nextDouble();
+        System.out.println("Ingrese la variable b");
+        b = In.nextDouble();
+        System.out.println("Ingrese la variable c");
+        c = In.nextDouble();
+        System.out.println("Ingrese la variable d");
+        d = In.nextDouble();
+        System.out.println("Ingrese la variable e");
+        e = In.nextDouble();
+        System.out.println("Ingrese la variable f");
+        f = In.nextDouble();  
+        System.out.println("Ingrese la variable g");
+        g = In.nextDouble();
+        System.out.println("Ingrese la variable h");
+        h = In.nextDouble();
+        
+        double ejercicioj = (3*a+b)/(c-(d+5*e)/(f+(g/2*h)));
+        System.out.println("El resultado del ejercicio j es: "+ejercicioj);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
