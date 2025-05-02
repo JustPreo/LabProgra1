ba5cf40966094a7ba6b7a913c16f3c894f9122fb
JustPreo <impre0hn@gmail.com>
2025-05-02 15:05:02 -0600

    TIEMPOOOOOOOOOOOOOOOOOOOOOOO

--- Semana2/ProyectosLaboratorio/src/proyectoslaboratorio/Cerrato_Aaron_Tiempo.java
+++ Semana2/ProyectosLaboratorio/src/proyectoslaboratorio/Cerrato_Aaron_Tiempo.java
@@ -3,11 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyectoslaboratorio;
+import java.util.Scanner;
 
 /**
  *
  * @author user
  */
 public class Cerrato_Aaron_Tiempo {
+    public static void main(String[]args)
+    {
+    int horaS = 3600;//1 hora = 3600 segundos
+    int minutoS = 60;//1 minuto = 60 segundos
+    //Valor no puede ser menor a 0
+    Scanner input = new Scanner(System.in);
+        System.out.println("Introducir los segundos a convertir a Horas,Miutos,Segundos:");
+        int numeroDigitado = input.nextInt();
+        
+        String mensajeValidacion = (numeroDigitado <= 0) ? "No se permite ese valor": "";
+        System.out.println(mensajeValidacion);
+        
+        if (numeroDigitado > 0)
+        {
+               int horasConvertidas = numeroDigitado/horaS;
+               int segundosRestantes = numeroDigitado % horaS;
+               int minutos = segundosRestantes / minutoS;
+               int segundos = segundosRestantes % minutoS;  
+               
+               String horaTexto = (horasConvertidas ==1) ? "hora" : "horas";
+               String minutosTexto = (minutos==1) ? "minuto":"minutos";
+               String segundosTexto = (segundos ==1) ? "segundo":"segundos";
+               System.out.println(horasConvertidas+" "+horaTexto+" "+minutos+" "+minutosTexto+" "+segundos+" "+segundosTexto);
+        }
+        
+        
+ 
+        
+        
+    
+    
+    
+    
+    
+    }
     
 }
