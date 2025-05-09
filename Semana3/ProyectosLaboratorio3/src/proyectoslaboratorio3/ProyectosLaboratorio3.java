8dc50dc9f8105b9d6e9e1be7bca63c7dc4ea7343
JustPreo <impre0hn@gmail.com>
2025-05-09 14:17:20 -0600

    Termine con lo de menu y opcion 1

---
+++ Semana3/ProyectosLaboratorio3/src/proyectoslaboratorio3/ProyectosLaboratorio3.java
@@ -0,0 +1,186 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoslaboratorio3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class ProyectosLaboratorio3 {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean seguirMenu = true;
+        Scanner input = new Scanner(System.in).useDelimiter("\n");
+        
+        
+        while (seguirMenu)
+        {
+            System.out.println("-----Menu-----"+
+                    "\n1.Palabra Alreves"+
+                    "\n2.Numero Perfecto"+
+                    "\n3.Primos"+
+                    "\n4.Votaciones"+
+                    "\n5.Salir");
+            System.out.println("Eliga una opcion:");
+            char opcion = input.next().charAt(0);
+            
+            switch (opcion)
+            {
+                case '1'://Palabra Alreves
+                    System.out.println("Cantidad de palabras a ingresar:");
+                    int nPalabras = input.nextInt();
+                    
+                    
+                    /*Palabra Alreves. Ingresar una cantidad de palabras y luego
+                    se ingresa una String desde el teclado, e imprimir al revés
+                    a cada palabra que se ingrese, y al final indicar cual ha sido
+                    la palabra mas larga y si es palíndromo (Considerar si existe 
+                    palabras con la misma cantidad de caracteres). */
+                    int longitudLarga = 0;
+                    String palabraLarga = "";
+                    String palabraAlrevesLarga = "";
+                    String texto = "";
+                    
+                    for (int i=1;i <=nPalabras;i++)
+                    {
+                        String palabraAlreves = "";
+                        System.out.println("Ingrese la palabra");
+                        String palabra = input.next();
+                        int longitud = palabra.length();
+                        
+                        for (int h=0;h<longitud;h++)
+                        {
+                            char letra = palabra.charAt(h);
+                            palabraAlreves = letra + palabraAlreves;
+                        
+                        }//Fin for h
+                        
+                        System.out.println("Palabra alreves:"+palabraAlreves);
+
+                        //----------------------------------------------
+                        if (longitud>longitudLarga)
+                        {
+                        
+                        
+                        longitudLarga = longitud;
+                        palabraLarga = palabra;
+                        palabraAlrevesLarga = palabraAlreves;
+                        
+                        texto = "La palabra mas larga es:"+palabraLarga+
+                            "\nLongitud:"+longitudLarga+
+                            "\nPalabra Alreves:"+palabraAlrevesLarga;
+                        
+                        if (palabraLarga.equalsIgnoreCase(palabraAlrevesLarga))//Revisa si es palindromo
+                        {
+                            texto= texto + "\nEs Palindromo";
+                            
+                        }
+                        
+                        }
+                        else if (longitud == longitudLarga)
+                        //Revisara si es la misma long y si es la misma comparara si son palindromo
+                        {
+                        texto = texto + "\nOtra palabra larga es:"+palabra+
+                            "\nLongitud:"+longitud+
+                            "\nPalabra Alreves:"+palabraAlreves;
+                        if (palabra.equalsIgnoreCase(palabraAlreves))//Revisa si es palindromo
+                        {
+                            texto= texto + "\nEs Palindromo";
+                            
+                        }
+                        }
+                        System.out.println("PRUEBA DE TESTEO \n"+texto);
+                    
+                    }//Fin for i
+                    
+                    
+                    System.out.println("-----------------------"+"\n"+texto);
+                    
+                    break;//Fin case
+                case '2'://Numero Perfecto
+                    
+                    break;
+                case '3'://Primos
+                    break;
+                case '4'://Votaciones
+                    System.out.println("Cuantos votantes hay en el pais?");
+                    int votantesTotal = input.nextInt();
+                    int votoAzul=0 , votoRojo=0 , votoNegro=0 , votoAmarillo=0,votoTotal=0,votoNulo=0;
+                    double votoPorcentaje=0,votoPorcentajeTotal=0;
+                    String ganador = "";
+                    //Iniciar variables
+                    
+                    for (int i = 0;i < votantesTotal;i++)
+                    {
+                        System.out.println("Ingrese por cual equipo votara:");
+                        System.out.println("AZUL | ROJO | NEGRO | AMARILLO");
+                        String voto = input.next().toLowerCase();
+                        
+                        switch (voto)
+                        {
+                            case "azul":
+                                votoAzul ++;
+                                votoTotal++;
+                                break;
+                            case "rojo":
+                                votoRojo ++;
+                                votoTotal++;
+                                break;
+                            case "negro":
+                                votoNegro++;
+                                votoTotal++;
+                                break;
+                            case "amarillo":
+                                votoAmarillo++;
+                                votoTotal++;
+                                break;
+                            default:
+                                votoNulo++;
+                                votoTotal++;
+                                break;
+                                
+                        }
+                        //TODO Determinar lo de 
+
+                    }//Fin for
+                    
+                        votoPorcentaje =votantesTotal * 0.6;
+                        if (votoTotal >= votoPorcentaje)
+                        {
+                            System.out.println("Gano:");
+                        
+                        }
+                    break;
+                case '5'://Salir
+                    seguirMenu = false;
+                    break;
+                default://Por si no ponen un char valido
+                    System.out.println("Ingrese una opcion valida");
+                    break;
+            
+            
+            
+            }
+        
+        
+        
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
